c97b75332dc9448a19bc452073d0cb329aeb29db
eypowxoa <126567098+eypowxoa@users.noreply.github.com>
2025-04-27 10:19:01 +0400

    Implemented getKeyPath

---
+++ src/ArrayAccessor.php
@@ -0,0 +1,232 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eypowxoa\ArrayAccessor;
+
+final class ArrayAccessor implements ArrayAccessorInterface
+{
+    public function __construct(
+        private readonly array $data,
+        private readonly string $path = '',
+    ) {}
+
+    public function getAssociative(
+        int|string $key
+    ): ArrayAccessorInterface {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getAssociativeOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?ArrayAccessorInterface {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getBool(
+        int|string $key,
+        int $flags = 0,
+    ): bool {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getBoolOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?bool {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getDate(
+        int|string $key,
+        int $flags = 0,
+        string ...$formats,
+    ): \DateTimeImmutable {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getDateOptional(
+        int|string $key,
+        int $flags = 0,
+        string ...$formats,
+    ): ?\DateTimeImmutable {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getEnum(
+        int|string $key,
+        string $class,
+        int $flags = 0,
+    ): object {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getEnumOptional(
+        int|string $key,
+        string $class,
+        int $flags = 0,
+    ): ?object {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getFalse(
+        int|string $key,
+        int $flags = 0,
+    ): false {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getFalseOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?false {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getFloat(
+        int|string $key,
+        int $flags = 0,
+        float $minimum = -PHP_FLOAT_MAX,
+        float $maximum = PHP_FLOAT_MAX,
+    ): float {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getFloatOptional(
+        int|string $key,
+        int $flags = 0,
+        float $minimum = -PHP_FLOAT_MAX,
+        float $maximum = PHP_FLOAT_MAX,
+    ): ?float {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getInstance(
+        int|string $key,
+        string $class,
+    ): object {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getInstanceOptional(
+        int|string $key,
+        string $class,
+        int $flags = 0,
+    ): ?object {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getInt(
+        int|string $key,
+        int $flags = 0,
+        int $minimum = PHP_INT_MIN,
+        int $maximum = PHP_INT_MAX,
+    ): int {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getIntOptional(
+        int|string $key,
+        int $flags = 0,
+        int $minimum = PHP_INT_MIN,
+        int $maximum = PHP_INT_MAX,
+    ): ?int {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getKeyPath(int|string $key): string
+    {
+        $escaped = $key;
+
+        if (\is_int($escaped)) {
+            $escaped = \sprintf('%d', $escaped);
+        }
+
+        $escaped = strtr($escaped, [
+            ' ' => '{s}',
+            "\n" => '{n}',
+            '{' => '{l}',
+            '}' => '{r}',
+        ]);
+
+        if ('' === $escaped) {
+            $escaped = '{e}';
+        }
+
+        if ('' === $this->path) {
+            return $escaped;
+        }
+
+        return $this->path.'.'.$escaped;
+    }
+
+    public function getKeys(): array
+    {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getList(
+        int|string $key
+    ): ArrayAccessorInterface {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getListOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?ArrayAccessorInterface {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getNull(
+        int|string $key,
+    ): null {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getNullOptional(
+        int|string $key,
+        int $flags = 0,
+    ): null {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getString(
+        int|string $key,
+        int $flags = 0,
+    ): string {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getStringOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?string {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getTrue(
+        int|string $key,
+        int $flags = 0,
+    ): true {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function getTrueOptional(
+        int|string $key,
+        int $flags = 0,
+    ): ?true {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function hasKey(int|string $key): bool
+    {
+        throw new \LogicException('Not implemented');
+    }
+
+    public function isNull(int|string $key): bool
+    {
+        throw new \LogicException('Not implemented');
+    }
+}
---
+++ tests/ArrayAccessorTest.php
@@ -0,0 +1,323 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eypowxoa\ArrayAccessor\Tests;
+
+use Eypowxoa\ArrayAccessor\ArrayAccessor;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+#[CoversClass(ArrayAccessor::class)]
+final class ArrayAccessorTest extends TestCase
+{
+    public static function provideGetAssociative(): array
+    {
+        return [];
+    }
+
+    public static function provideGetAssociativeOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetBool(): array
+    {
+        return [];
+    }
+
+    public static function provideGetBoolOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetDate(): array
+    {
+        return [];
+    }
+
+    public static function provideGetDateOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetEnum(): array
+    {
+        return [];
+    }
+
+    public static function provideGetEnumOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetFalse(): array
+    {
+        return [];
+    }
+
+    public static function provideGetFalseOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetFloat(): array
+    {
+        return [];
+    }
+
+    public static function provideGetFloatOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetInstance(): array
+    {
+        return [];
+    }
+
+    public static function provideGetInstanceOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetInt(): array
+    {
+        return [];
+    }
+
+    public static function provideGetIntOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetKeyPath(): array
+    {
+        return [
+            ['', '', '{e}'],
+            ['', "\n", '{n}'],
+            ['', "\n\n", '{n}{n}'],
+            ['', ' ', '{s}'],
+            ['', '  ', '{s}{s}'],
+            ['', '{', '{l}'],
+            ['', '{{', '{l}{l}'],
+            ['', '}', '{r}'],
+            ['', '}}', '{r}{r}'],
+            ['', '\\', '\\'],
+            ['', 'key', 'key'],
+            ['', "{\n \\key  \n\n}}", '{l}{n}{s}\key{s}{s}{n}{n}{r}{r}'],
+            ["{\n \\key  \n\n}}", '', "{\n \\key  \n\n}}.{e}"],
+            ["{\n \\key  \n\n}}", "\n", "{\n \\key  \n\n}}.{n}"],
+            ["{\n \\key  \n\n}}", "\n\n", "{\n \\key  \n\n}}.{n}{n}"],
+            ["{\n \\key  \n\n}}", ' ', "{\n \\key  \n\n}}.{s}"],
+            ["{\n \\key  \n\n}}", '  ', "{\n \\key  \n\n}}.{s}{s}"],
+            ["{\n \\key  \n\n}}", '{', "{\n \\key  \n\n}}.{l}"],
+            ["{\n \\key  \n\n}}", '{{', "{\n \\key  \n\n}}.{l}{l}"],
+            ["{\n \\key  \n\n}}", '}', "{\n \\key  \n\n}}.{r}"],
+            ["{\n \\key  \n\n}}", '}}', "{\n \\key  \n\n}}.{r}{r}"],
+            ["{\n \\key  \n\n}}", '\\', "{\n \\key  \n\n}}.\\"],
+            ["{\n \\key  \n\n}}", 'key', "{\n \\key  \n\n}}.key"],
+            ["{\n \\key  \n\n}}", "{\n \\key  \n\n}}", "{\n \\key  \n\n}}.{l}{n}{s}\\key{s}{s}{n}{n}{r}{r}"],
+        ];
+    }
+
+    public static function provideGetKeys(): array
+    {
+        return [];
+    }
+
+    public static function provideGetList(): array
+    {
+        return [];
+    }
+
+    public static function provideGetListOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetNull(): array
+    {
+        return [];
+    }
+
+    public static function provideGetNullOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetString(): array
+    {
+        return [];
+    }
+
+    public static function provideGetStringOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideGetTrue(): array
+    {
+        return [];
+    }
+
+    public static function provideGetTrueOptional(): array
+    {
+        return [];
+    }
+
+    public static function provideHasKey(): array
+    {
+        return [];
+    }
+
+    public static function provideIsNull(): array
+    {
+        return [];
+    }
+
+    public function testGetAssociative(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetAssociativeOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetBool(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetBoolOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetDate(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetDateOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetEnum(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetEnumOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetFalse(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetFalseOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetFloat(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetFloatOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetInstance(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetInstanceOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetInt(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetIntOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    #[DataProvider('provideGetKeyPath')]
+    public function testGetKeyPath(string $path, string $key, string $expected): void
+    {
+        self::assertSame($expected, (new ArrayAccessor([], $path))->getKeyPath($key));
+    }
+
+    public function testGetKeys(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetList(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetListOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetNull(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetNullOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetString(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetStringOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetTrue(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testGetTrueOptional(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testHasKey(): void
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testIsNull(): void
+    {
+        self::markTestIncomplete();
+    }
+}
